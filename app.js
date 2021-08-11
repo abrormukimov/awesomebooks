f58ca476f1d342eb559f74fbfc2c20c5b382dd71
abrormukimov <abrormm34@gmail.com>
2021-08-11 13:46:11 +0500

    Refactor to use classes instead of Object constructors

--- app.js
+++ app.js
@@ -5,66 +5,68 @@ function qs(element) {
 const addButton = qs('.add-btn');
 const ul = qs('.ul');
 
-function getFromLocalStorage() {
-  const localstore = JSON.parse(localStorage.getItem('localstore')) || [];
-  return localstore;
-}
-
-function setToLocalStorage(book) {
-  const localstore = getFromLocalStorage();
-  localstore.push(book);
-  localStorage.setItem('localstore', JSON.stringify(localstore));
-}
-
-function removeBook(title1) {
-  const localstore = getFromLocalStorage();
-  for (let i = 0; i < localstore.length; i += 1) {
-    if (localstore[i].title === title1) {
-      localstore.splice(i, 1);
-    }
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-  localStorage.setItem('localstore', JSON.stringify(localstore));
-}
 
-function clearFields() {
-  const title = qs('.form-title');
-  const author = qs('.form-author');
-  title.value = '';
-  author.value = '';
-}
+  static addBookToLibrary(book) {
+    const li = document.createElement('li');
+    const titleSpan = document.createElement('span');
+    titleSpan.innerHTML = `${book.title} `;
+    const authorSpan = document.createElement('span');
+    authorSpan.innerHTML = `${book.author} `;
+    const removeButton = document.createElement('button');
+    const hr = document.createElement('hr');
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
+    titleSpan.classList.add('book-title');
+    authorSpan.classList.add('book-author');
+    removeButton.classList.add('remove-btn');
+    removeButton.setAttribute('data-title', `${book.title}`);
+    removeButton.innerHTML = 'Remove';
 
-function addBookToLibrary(book) {
-  const li = document.createElement('li');
-  const titleSpan = document.createElement('span');
-  titleSpan.innerHTML = `${book.title} `;
-  const authorSpan = document.createElement('span');
-  authorSpan.innerHTML = `${book.author} `;
-  const removeButton = document.createElement('button');
-  const hr = document.createElement('hr');
+    li.appendChild(titleSpan);
+    li.appendChild(authorSpan);
+    li.appendChild(removeButton);
+    li.appendChild(hr);
 
-  titleSpan.classList.add('book-title');
-  authorSpan.classList.add('book-author');
-  removeButton.classList.add('remove-btn');
-  removeButton.setAttribute('data-title', `${book.title}`);
-  removeButton.innerHTML = 'Remove';
+    ul.appendChild(li);
+  }
 
-  li.appendChild(titleSpan);
-  li.appendChild(authorSpan);
-  li.appendChild(removeButton);
-  li.appendChild(hr);
+  static getFromLocalStorage() {
+    const localstore = JSON.parse(localStorage.getItem('localstore')) || [];
+    return localstore;
+  }
 
-  ul.appendChild(li);
+  static setToLocalStorage(book) {
+    const localstore = Book.getFromLocalStorage();
+    localstore.push(book);
+    localStorage.setItem('localstore', JSON.stringify(localstore));
+  }
+
+  static removeBook(title1) {
+    const localstore = Book.getFromLocalStorage();
+    for (let i = 0; i < localstore.length; i += 1) {
+      if (localstore[i].title === title1) {
+        localstore.splice(i, 1);
+      }
+    }
+    localStorage.setItem('localstore', JSON.stringify(localstore));
+  }
+
+  static clearFields() {
+    const title = qs('.form-title');
+    const author = qs('.form-author');
+    title.value = '';
+    author.value = '';
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const fromStorage = getFromLocalStorage();
+  const fromStorage = Book.getFromLocalStorage();
   fromStorage.forEach((book) => {
-    addBookToLibrary(book);
+    Book.addBookToLibrary(book);
   });
 });
 
@@ -73,15 +75,15 @@ addButton.addEventListener('click', (e) => {
   const title = qs('.form-title').value;
   const author = qs('.form-author').value;
   const book = new Book(title, author);
-  addBookToLibrary(book);
-  setToLocalStorage(book);
-  clearFields();
+  Book.addBookToLibrary(book);
+  Book.setToLocalStorage(book);
+  Book.clearFields();
 });
 
 ul.addEventListener('click', (e) => {
   if (e.target.classList.contains('remove-btn')) {
     e.target.parentElement.remove();
     const title = e.target.getAttribute('data-title');
-    removeBook(title);
+    Book.removeBook(title);
   }
 });
